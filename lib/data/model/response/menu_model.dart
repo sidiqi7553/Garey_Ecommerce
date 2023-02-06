de7f7b896b8ddd12e8aa7eeb9b89c9b8ee3a7edf
ahmad-ali3158 <113877531+ahmad-ali3158@users.noreply.github.com>
2023-02-05 23:03:06 -0800

    dodo

--- lib/data/model/response/menu_model.dart
+++ lib/data/model/response/menu_model.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
-
-class MenuModel {
-  String icon;
-  String title;
-  String route;
-
-  MenuModel({@required this.icon, @required this.title, @required this.route});
-}
+// import 'package:flutter/material.dart';
+//
+// class MenuModel {
+//   String icon;
+//   String title;
+//   String route;
+//
+//   MenuModel({@required this.icon, @required this.title, @required this.route});
+// }
